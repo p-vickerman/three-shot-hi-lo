4582101d0fff4ee6485d5518cf7a0179634db34c
p-vickerman <patrick@vickerman.net>
2022-06-01 12:58:29 -0400

    adding files v1

---
+++ main.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt-sync')({sigint:true})
+
+let number = Math.floor(Math.random()*100) + 1
+
+let guess = Number(prompt("I'm thinking of a number between 1 and 100. Try to guess it: "));
+
+let attempt = 1;
+
+
+
+while (guess !==number  && attempt < 3) {
+    attempt++;
+    if (guess > number){
+        guess= Number(prompt("Sorry, too high! Guess again."))
+    } if (guess < number){
+        guess= Number(prompt("Sorry, too low! Guess again."))
+    } if (attempt === 3){
+        console.log("Sorry, I was thinking of " + number)
+    }
+
+}
+
+if (guess === number){
+    console.log("Congratulations" + number + " is correct!");
+    }
